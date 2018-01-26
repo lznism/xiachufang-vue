a4d215f084b1d17a860c6085c298919c686804e1
lznism <lzguo@whu.edu.cn>
2018-01-27 00:28:21 +0800

    添加REM布局

--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './assets/styles/reset.scss';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import rem from './assets/lib/rem';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+rem(false, 200);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+});
