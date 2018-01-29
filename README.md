39f2c5354a87e611c0e27fa3a702735d425e9737
lznism <lzguo@whu.edu.cn>
2018-01-29 09:03:21 +0800

    添加fastclick

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 #### 技术栈
-本项目采用`vue+vuex+vue-router`技术栈，为业余前端练手项目
+本项目采用`vue+vuex+vue-router`技术栈，为业余前端练手项目，项目框架采用`vue-cli`构建   
+
+#### 项目注意点
+1. 引入fastclick
+2. 采用[rem布局](http://www.cnblogs.com/guolizhi/p/8342569.html)
 
 #### 说明
 本项目模仿[下厨房](http://m.xiachufang.com)构建
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import rem from './assets/lib/rem';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import fastclick from 'fastclick';
 
 Vue.config.productionTip = false;
 rem(false, 69.44444444);
+fastclick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
