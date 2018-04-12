81bb21a047b3aa02f7021e2fc4690b2ce11c91ef
lznism <lzguo@whu.edu.cn>
2018-04-12 20:44:37 +0800

    防止在搜索结果页面刷新丢失数据

--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -6,7 +6,7 @@
     </div>
 </template>
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 import Search from '@/components/search';
 import MenuList from '@/components/menu-list';
 import XcfHeader from '@/components/header';
@@ -18,6 +18,16 @@ export default {
     },
     computed: {
         ...mapGetters(['searchResults'])
+    },
+    created() {
+        // 防止在搜索结果页面刷新，丢失数据
+        if (this.searchResults.length === 0) {
+            let keyword = this.$route.query.keyword;
+            this.getSearch(keyword);
+        }
+    },
+    methods: {
+        ...mapActions(['getSearch'])
     }
 };
 </script>
